5abbaafda0eb989d42175dd45b0deb29e226dd9f
0dayx16 <91608871+ayaanlehashi11@users.noreply.github.com>
2024-01-01 06:26:57 -0800

    Update TrainNN.java

--- src/main/java/com/ayaanle/TrainNN.java
+++ src/main/java/com/ayaanle/TrainNN.java
@@ -1,29 +1,26 @@
 
 import impl.TrainNNImpl;
-import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfInt;
 import org.opencv.dnn.Dnn;
+import org.opencv.dnn.Layer;
 import org.opencv.dnn.Net;
 
 import java.io.File;
-import java.io.FilePermission;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.List;
 
 public class TrainNN  implements TrainNNImpl {
     public static int allowed_model_size;
-
     List<String> layers;
     MatOfInt outliers;
     public TrainNN(long add)
     {
         super();
-
     }
     File f;
     @Override
-    public boolean check_model(String model_path)
+    public boolean model_exists(String model_path)
     {
         f = new File(model_path);
         if(!f.exists())
@@ -37,13 +34,13 @@ public class TrainNN  implements TrainNNImpl {
     {
         f = new File(path);
         Path p = f.toPath();
-        if (check_model(path)) {
+        if (model_exists(path)) {
             if (f.canRead() && p.endsWith(".h5"))
                 return f.getName();
-       throw new FileNotFoundException();
+            else System.out.println("the file that have specified doesn't seem to be a model");
         }
+        else throw new FileNotFoundException();
         return "";
-
     }
     @SuppressWarnings("CallToPrintStackTrace")
     @Override
@@ -51,27 +48,47 @@ public class TrainNN  implements TrainNNImpl {
     {
         try {
             String mod =  model_info(path);
+            if(!mod.isEmpty())
+                System.out.println("model : " + mod);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
     @Override
-    public int NNTrain(boolean model_exist , String path)
+    public String NNTrain(String path)
     {
-        Net net = Dnn.readNetFromTorch(path);
-        if(!(net.empty())) {
-            outliers = net.getUnconnectedOutLayers();
-            List<String> layer_names = net.getLayerNames();
-            for(String layer_name : layer_names)
-            {
-                if(!layer_name.isEmpty())
-                {
-                    System.out.println(layer_name);
+        if (model_exists(path)) {
+            Net net = Dnn.readNetFromONNX(path);
+            net.
+            if (!(net.empty())) {
+                outliers = net.getUnconnectedOutLayers();
+                List<String> layer_names = net.getLayerNames();
+                for (int i = 0; i < layer_names.size(); i++) {
+                    System.out.println(net.getParam(layer_names.get(i)));
+                    return layer_names.get(i);
                 }
-                
             }
         }
-        return 0;
+        return "null";
+    }
+    String LayerID(String path)
+    {
+        List<Layer> layers;
+        int layer_id = 0;
+        if (model_exists(path)) {
+            Net net = Dnn.readNetFromONNX(path);
+            if (!(net.empty())) {
+                outliers = net.getUnconnectedOutLayers();
+                List<String>layer_names = net.getLayerNames();
+                for(int i = 0; i < layer_names.size(); i++)
+                {
+                    layer_id =  net.getLayerId(layer_names.get(i));
+                }
+
+
+            }
+        }
+        return String.valueOf(layer_id);
     }
 
 }
